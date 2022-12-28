e9ff3ebcf42f30fd92f5e7d8211ba3ea3aee7728
psydelle <70809908+psydelle@users.noreply.github.com>
2022-12-28 08:16:39 +0000

    added code for pickling the the colloc2BERT dictionary

--- testing.py
+++ testing.py
@@ -12,44 +12,86 @@
 
 ## COMMENTS -------------------------------------------------------------------
 
-# this file contains the code for the Minerva2 model, a simulation model
+# this file contains the code for the MINERVA2 model, a simulation model
 # of human memory, which we are using to investigate collocational processing. 
-# The model is based on the Minerva model by Hintzmann (1986)
+# The model accounts for data from both episodic and semantic memory from a single 
+# system. Theoretically speaking, the model is concerned with long-term memory
+# while also assuming a short-term memory system that can communicate with the 
+# long-term memory system. The two memory systems communicate with each other. 
+# The short-term memory system can send a "probe" to the long-term memory system
+# and the long-term memory system can reply with an "echo". 
 
 #-----------------------------------------------------------------------------#
 
+## ACKNOWLEDGEMENTS  ----------------------------------------------------------
 
+# Ivan Vegner
+# 
 
-import random
-print('imported random')
-import pandas as pd
-print('imported pandas')
-from exract_embeddings import *
-print('imported extract_embeddings')
+#-----------------------------------------------------------------------------#
+
+## Set-Up ---------------------------------------------------------------------
+import torch # for tensors
+import random # for random number generation
+import pandas as pd # for dataframe manipulation
+import os  # for file management
+import pickle # for saving and loading objects
+from transformers import AutoTokenizer, AutoModel
+from exract_embeddings import get_word_vector # for BERT embeddings
+
+# set random seeds for reproducibility
+
+random.seed(40)
+torch.manual_seed(40)
 
 ## read in the dataset
-#
-df = pd.read_csv("FinalDataset.csv")
-dataset = list(df['item'])[0:10]
+
+df = pd.read_csv("FinalDataset.csv") # same dataset as MSc project
+dataset = list(df['item']) # list of items
 print('loaded the dataset')
 
-def get_bert(mod_name="distilbert-base-uncased"):
-    tokenizer = AutoTokenizer.from_pretrained(mod_name)
-    model = AutoModel.from_pretrained(mod_name, output_hidden_states=True)
-    return tokenizer, model
+M = 10000 
 
-def grab_bert(colloc, model, tokenizer, layers = [-4, -3, -2, -1]):
-    return get_word_vector(colloc, tokenizer, model, layers) 
+if not os.path.isfile('colloc2BERT.dat'):
+
+    # set up the model
+    #
+    def get_bert(mod_name="distilbert-base-uncased"): 
+        tokenizer = AutoTokenizer.from_pretrained(mod_name)
+        model = AutoModel.from_pretrained(mod_name, output_hidden_states=True) 
+        return tokenizer, model
+
+    def grab_bert(colloc, model, tokenizer, layers = [-4, -3, -2, -1]):
+        return get_word_vector(colloc, tokenizer, model, layers) 
+
+    # grab BERT embeddings for the items in the dataset
+    #
+    
+    colloc2BERT = dict()
+    tokenizer, model = get_bert() 
+
+    for item in dataset:
+        print('dealing with this shit: ', item, '')
+        colloc2BERT[item] = grab_bert(item, model, tokenizer) 
+
+    # write the embeddings dictionary to a file to be re-used next time we run the code
+        #
+    colloc2BERTfile = open('colloc2BERT.dat', 'wb')
+    pickle.dump(colloc2BERT, colloc2BERTfile)
+    colloc2BERTfile.close()
+    print("Dictionary written  to file\n")
+
+else:
+    # get the previously calculated embeddings from the file in which they were stored
+    #
+    colloc2BERTfile = open('colloc2BERT.dat', 'rb')
+    colloc2BERT = pickle.load(colloc2BERTfile)
+    colloc2BERTfile.close()   
+    print("Read from file\n") 
 
 
-M = 10000
-colloc2BERT = dict()
-tokenizer, model = get_bert()
-for collocation in dataset:
-    print('dealing with this shit: ', collocation, '')
-    colloc2BERT[collocation] = grab_bert(collocation, model, tokenizer)
+colloc_bert_embeddings = torch.stack(list(colloc2BERT.values())) # stack the embeddings into a tensor
 
-colloc_bert_embeddings = torch.stack(list(colloc2BERT.values()))
 # sample from the collocations to make a M x 768 matrix
 sampled_collocs = torch.stack(random.choices(colloc_bert_embeddings, k=M-len(colloc_bert_embeddings)))
 matrix = torch.concat([colloc_bert_embeddings, sampled_collocs], dim=0)
@@ -58,6 +100,7 @@ assert matrix.size() == (M, 768), "Huh?"
 # Ivan's pedantic memory optimizations: since the concat we do not need the 
 # original tensors anymore because they have been copied when matrix was made
 del colloc_bert_embeddings, sampled_collocs
+
 
 
 # Obsolete, does the same as above:
@@ -77,12 +120,13 @@ del colloc_bert_embeddings, sampled_collocs
 #     matrix[i+len(colloc2BERT), :] = v
 
 
-#### Now we got to add some noise to the memory matrix (paramerer L)
-L = 0.6 # cuz this is what the meta paper says
+#### Now we got to add some noise to the memory matrix (parameter L)
+L = 0.6 # 0.6 is what the meta paper says
 # noise between 0 and 1
-noise = torch.rand((M, 768))
 
-noisy_mem = torch.where(noise < L, torch.zeros((M, 768)), matrix)
+noise = torch.rand((M, 768)) # noise is a tensor of random numbers between 0 and 1
+
+noisy_mem = torch.where(noise < L, torch.zeros((M, 768)), matrix) # if the noise is less than L, then the memory is zero, otherwise it is the original matrix
 
 print(noisy_mem, noisy_mem.shape)
 
@@ -90,7 +134,6 @@ class Minerva2(object):
     '''
     This is a class for the Minerva2 model
     '''
-
     def __init__(self, F=None, M=None, Mat=None):
         if Mat is not None:
             self.Mat = Mat
@@ -101,26 +144,28 @@ class Minerva2(object):
     
     def probe(self, probe, tau=1.0):
         similarity = torch.cosine_similarity(probe, self.Mat, dim=0)
+        #similarity is always the same tensor why???? 
         activation = similarity**tau
         return activation
 
-    def recognize(self, probe, tau=1.0, k=0.99, maxiter=1000):
+    def recognize(self, probe, tau=1.0, k=0.99, maxiter=4):
         activation = self.probe(probe, tau)
-        if torch.argmax(activation) < k and tau < maxiter:
-            self.recognize(probe, tau+1, k, maxiter)
-        else:
-            return torch.max(activation), tau
+        big = torch.max(activation)
+        if big < k and tau < maxiter: 
+            activation, tau = self.recognize(probe, tau+1, k, maxiter)
+            #self.recognize(probe, tau+1, k, maxiter)
+        return torch.max(activation), tau
         
 
 
-minz = Minerva2(Mat=noisy_mem)
-act, rt = minz.recognize(colloc2BERT['recall vow'])
-print(act, rt)
+minz = Minerva2(Mat=noisy_mem) 
+output = [] 
 
 
-
-
-
-
-
+for item, vector in colloc2BERT.items():
+    act, rt = minz.recognize(vector)
+    print("appending to output" + item)
+    output.append([item, act, rt])
+#act, rt = minz.recognize(colloc2BERT['chase dream'])
+print(output)
 
