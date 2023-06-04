5a0c2cfd1011b9ae700089195db038227b9fd0ca
Ivan Vegner <ivan.vegner@ed.ac.uk>
2023-06-04 15:24:05 +0100

    Scripts to run one and all experiments

---
+++ src/run_all_experiments.py
@@ -0,0 +1,83 @@
+import argparse
+from pathlib import Path
+
+import pandas as pd
+from run_one_experiment import run_experiment
+
+# Run all experiments
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--dataset_to_use", help="Dataset to use", default="data/stimuli.csv")
+    parser.add_argument(
+        "-n",
+        "--num_participants",
+        help="How many participants to model?",
+        default=99,
+        type=int,
+    )
+    parser.add_argument(
+        "--en_pt_trans_pickle",
+        help="Path to pickle file containing pt translations of en embeddings (required for aligned space_lang)",
+        default=None,
+    )
+    parser.add_argument(
+        "--freq_fraction_pt",
+        help="Use this fraction of PT in frequency mixing (only applicable for mixed? aligned? space_lang, default 0.6)",
+        default=0.6,
+        type=float,
+    )
+    parser.add_argument(
+        "--minerva_k",
+        help="Minerva k (threshold) parameter",
+        default=0.955,
+        type=float,
+    )
+    parser.add_argument(
+        "--minerva_max_iter",
+        help="Minerva max_iter parameter",
+        default=450,
+        type=int,
+    )
+    parser.add_argument(
+        "--num_workers",
+        help="Number of workers to use",
+        default=4,
+        type=int,
+    )
+    parser.add_argument(
+        "--no_concat_tokens",
+        dest="concat_tokens",
+        help="Concatenate BERT tokens instead of averaging",
+        action="store_false",
+        default=True,
+    )
+    parser.add_argument(
+        "--label",
+        help="Arbitrary label to append to all files created",
+        default=None,
+    )
+    args = parser.parse_args()
+
+    results_dfs = []
+    for space_lang in ["en", "pt"]:
+        for frequency_lang in ["en", "pt", "mixed"]:
+            print(f"Running experiment with space_lang={space_lang} and frequency_lang={frequency_lang}")
+            results_df = run_experiment(
+                dataset_to_use=args.dataset_to_use,
+                space_lang=space_lang,
+                frequency_lang=frequency_lang,
+                num_participants=args.num_participants,
+                en_pt_trans_pickle=args.en_pt_trans_pickle,
+                freq_fraction_pt=args.freq_fraction_pt,
+                minerva_k=args.minerva_k,
+                minerva_max_iter=args.minerva_max_iter,
+                num_workers=args.num_workers,
+                concat_tokens=args.concat_tokens,
+                label=args.label,
+            )
+
+            results_dfs.append(results_df)
+
+    results_df = pd.concat(results_dfs)
+    out_file = f"results/combo_results-{Path(args.dataset_to_use).name[:-4]}-{args.num_participants}p-{f'-mix{args.freq_fraction_pt}'}{'-concat' if args.concat_tokens else ''}-m2k_{args.minerva_k}-m2mi_{args.minerva_max_iter}{'-' + args.label if args.label else ''}.csv"
+    results_df.to_csv(out_file, index=False)
--- src/run_one_experiment.py
+++ src/run_one_experiment.py
@@ -32,6 +32,7 @@
 # -----------------------------------------------------------------------------#
 
 ## Set-Up ---------------------------------------------------------------------
+from typing import Optional
 import torch  # for tensors
 import random  # for random number generation
 import pandas as pd  # for dataframe manipulation
@@ -43,7 +44,6 @@ import numpy as np
 from pathlib import Path
 
 import csv as csv  # for reading in the dataset, etc.
-import os  # for file management
 from joblib import Parallel, delayed  # for parallel processing
 from filelock import FileLock
 import argparse
@@ -61,72 +61,6 @@ np.random.seed(0)
 # set current working directory to this folder
 
 # os.chdir(Path(__file__).parent.absolute())
-
-parser = argparse.ArgumentParser()
-parser.add_argument("-d", "--dataset_to_use", help="Dataset to use", default="data/stimuli.csv")
-parser.add_argument(
-    "-l",
-    "--space_lang",
-    help="Use embeddings from this language space (en, pt, en_aligned)",
-    default="en",
-    choices=["en", "pt", "en_aligned"],
-)
-parser.add_argument(
-    "-f",
-    "--frequency_lang",
-    help="Use frequency counts from this language (en, pt or mix)",
-    default="en",
-    choices=["en", "pt", "mix"],
-)
-parser.add_argument(
-    "-n",
-    "--num_participants",
-    help="How many participants to model?",
-    default=99,
-    type=int,
-)
-parser.add_argument(
-    "--en_pt_trans_pickle",
-    help="Path to pickle file containing pt translations of en embeddings (required for aligned space_lang)",
-    default=None,
-)
-parser.add_argument(
-    "--freq_fraction_pt",
-    help="Use this fraction of PT in frequency mixing (only applicable for aligned space_lang, default 0.6)",
-    default=0.6,
-    type=float,
-)
-parser.add_argument(
-    "--minerva_k",
-    help="Minerva k (threshold) parameter",
-    default=0.955,
-    type=float,
-)
-parser.add_argument(
-    "--minerva_max_iter",
-    help="Minerva max_iter parameter",
-    default=450,
-    type=int,
-)
-parser.add_argument(
-    "--num_workers",
-    help="Number of workers to use",
-    default=4,
-    type=int,
-)
-parser.add_argument(
-    "--no_concat_tokens",
-    dest="concat_tokens",
-    help="Concatenate BERT tokens instead of averaging",
-    action="store_false",
-    default=True,
-)
-parser.add_argument(
-    "--label",
-    help="Arbitrary label to append to all files created",
-    default=None,
-)
-args = parser.parse_args()
 
 EN_BERT = "distilbert-base-cased"
 PT_BERT = "adalbertojunior/distilbert-portuguese-cased"
@@ -177,18 +111,31 @@ class Minerva2(object):
 
 ##-----------------------------------------------------------------------------##
 
-if __name__ == "__main__":
+
+def run_experiment(
+    dataset_to_use: str,
+    space_lang: str,
+    frequency_lang: str,
+    num_participants: int,
+    en_pt_trans_pickle: Optional[str] = None,
+    freq_fraction_pt: Optional[float] = None,
+    minerva_k=0.955,
+    minerva_max_iter=450,
+    num_workers=1,
+    concat_tokens=False,
+    label=None,
+):
     ## read in the dataset
-    if Path(args.dataset_to_use).name == "stimuli.csv":
-        df = pd.read_csv(args.dataset_to_use)  # same dataset as MSc project
+    if Path(dataset_to_use).name == "stimuli.csv":
+        df = pd.read_csv(dataset_to_use)  # same dataset as MSc project
         # fcoll = list(df['fcoll'].str.replace(r'\D', '')) # collocation frequencies
         dataset = df[["item", "item_pt"]]
-        # if args.space_lang in ["en", "en_aligned"]:
+        # if space_lang in ["en", "en_aligned"]:
         #     dataset = list(df['item']) # list of items
-        # elif args.space_lang == "pt":
+        # elif space_lang == "pt":
         #     dataset = list(df['item_pt']) # list of items
         # else:
-        #     raise NotImplementedError(f"{args.space_lang} space lang not implemented yet")
+        #     raise NotImplementedError(f"{space_lang} space lang not implemented yet")
 
         # convert comma-sep number strings to numbers
         _coll_freq_en = df[["fitem", "fnode", "fcoll"]].applymap(
@@ -204,14 +151,14 @@ if __name__ == "__main__":
             _coll_freq_pt["fitempt"], _coll_freq_pt["fnodept"], _coll_freq_pt["fcollpt"]
         )
 
-        # if args.frequency_lang == "en":
+        # if frequency_lang == "en":
         #     fcoll = norm_freq_en
-        # elif args.frequency_lang == "pt":
+        # elif frequency_lang == "pt":
         #     fcoll = norm_freq_pt
-        # elif args.frequency_lang == "mix":
+        # elif frequency_lang == "mix":
         #     raise NotImplementedError("Mix not implemented yet hehe")
 
-    elif args.dataset_to_use == "FinalDataset.csv":
+    elif dataset_to_use == "FinalDataset.csv":
         raise NotImplementedError(
             "FinalDataset.csv doesn't yet support any of the parameters you're probably trying to use"
         )
@@ -223,7 +170,7 @@ if __name__ == "__main__":
 
     M = 10000
 
-    bert_embeddings_cache_filename = f'data/processed/colloc2BERT-{Path(args.dataset_to_use).name[:-4]}-lang_{args.space_lang}{"-concat" if args.concat_tokens else ""}{"-" + args.label if args.label else ""}.dat'
+    bert_embeddings_cache_filename = f'data/processed/colloc2BERT-{Path(dataset_to_use).name[:-4]}-lang_{space_lang}{"-concat" if concat_tokens else ""}{"-" + label if label else ""}.dat'
     if not os.path.isfile(bert_embeddings_cache_filename):
         # set up the model and tokenizer for BERT embeddings
         def get_bert(mod_name="distilbert-base-uncased"):
@@ -233,39 +180,37 @@ if __name__ == "__main__":
 
         def grab_bert(colloc, model, tokenizer, layers=[-4, -3, -2, -1]):
             # TODO: use only one layer
-            return get_word_vector(
-                colloc, tokenizer, model, layers, concat_tokens=args.concat_tokens
-            )
+            return get_word_vector(colloc, tokenizer, model, layers, concat_tokens=concat_tokens)
 
         # grab BERT embeddings for the items in the dataset
         colloc2BERT = dict()
-        if args.space_lang == "en":
+        if space_lang == "en":
             tokenizer, model = get_bert(EN_BERT)
-        elif args.space_lang == "pt":
+        elif space_lang == "pt":
             tokenizer, model = get_bert(PT_BERT)
-        elif args.space_lang == "en_aligned":
+        elif space_lang == "en_aligned":
             # en_aligned is using en embeddings aligned to pt space, i.e., replace en embeddings with en in pt space
-            with open(args.en_pt_trans_pickle, "rb") as f:
+            with open(en_pt_trans_pickle, "rb") as f:
                 en_pt_dict = pickle.load(f)
         else:
             raise Exception("Weird space lang")
 
-        # if "aligned" in args.space_lang:
-        #     alignment_net = CollocNet(embed_dim=768 if not args.concat_tokens else 768*2)
+        # if "aligned" in space_lang:
+        #     alignment_net = CollocNet(embed_dim=768 if not concat_tokens else 768*2)
 
         # for item in dataset:
-        #     if args.space_lang == "en_aligned":
+        #     if space_lang == "en_aligned":
         #         vec = alignment_net.en_to_pt(vec)
-        #     elif args.space_lang == "pt_aligned":
+        #     elif space_lang == "pt_aligned":
         #         vec = alignment_net.pt_to_en(vec)
 
         for item_en, item_pt in zip(dataset["item"], dataset["item_pt"]):
-            item = item_en if args.space_lang in ["en", "en_aligned"] else item_pt
+            item = item_en if space_lang in ["en", "en_aligned"] else item_pt
 
             print(
-                f'Retrieving vector for "{item}" from {"dictionary" if "aligned" in args.space_lang else model.config._name_or_path}'
+                f'Retrieving vector for "{item}" from {"dictionary" if "aligned" in space_lang else model.config._name_or_path}'
             )
-            if args.space_lang == "en_aligned":
+            if space_lang == "en_aligned":
                 # just get pt projection from dictionary
                 vec = torch.tensor(en_pt_dict[item]["pt"])
             else:
@@ -289,8 +234,8 @@ if __name__ == "__main__":
         colloc2BERTfile.close()
         print(f"Read from file {bert_embeddings_cache_filename}")
 
-    # if args.en_pt_trans_pickle:
-    #     with open(args.en_pt_trans_pickle, "rb") as f:
+    # if en_pt_trans_pickle:
+    #     with open(en_pt_trans_pickle, "rb") as f:
     #         en_pt_dict = pickle.load(f)
     #     for k in colloc2BERT:
     #         # print(np.array(colloc2BERT[k])[:10], np.array(en_pt_dict[k]["en"])[:10])
@@ -309,7 +254,7 @@ if __name__ == "__main__":
 
     ## Let's run our experiment. First we generate random seeds to simulate
     ## 99 l1 participants from Souza and Chalmers (2021)
-    n = args.num_participants  # sample size
+    n = num_participants  # sample size
     p = 0
     seed = []
     for s in range(n):
@@ -317,7 +262,7 @@ if __name__ == "__main__":
 
     ## Now we run the experiment
 
-    def iter(p, s, out_filename, device):
+    def iter(p, s, device):
         # print(f"\nSeed {s}\n")
         random_generator = random.Random(s)
         torch_generator = torch.Generator().manual_seed(s)
@@ -325,16 +270,16 @@ if __name__ == "__main__":
         # sample from the collocations to make a M x 768 matrix
         sample_k = M - len(colloc_bert_embeddings)
 
-        if args.frequency_lang == "en":
+        if frequency_lang == "en":
             sampled_collocs = torch.stack(
                 random_generator.choices(colloc_bert_embeddings, k=sample_k, weights=norm_freq_en)
             )
-        elif args.frequency_lang == "pt":
+        elif frequency_lang == "pt":
             sampled_collocs = torch.stack(
                 random_generator.choices(colloc_bert_embeddings, k=sample_k, weights=norm_freq_pt)
             )
-        elif args.frequency_lang == "mix":
-            sample_k_pt = round(sample_k * args.freq_fraction_pt)
+        elif frequency_lang == "mix":
+            sample_k_pt = round(sample_k * freq_fraction_pt)
             sample_k_en = sample_k - sample_k_pt
             _sampled_collocs_pt = torch.stack(
                 random_generator.choices(
@@ -351,7 +296,7 @@ if __name__ == "__main__":
         matrix = torch.concat([colloc_bert_embeddings, sampled_collocs], dim=0)
 
         embed_dim = 768
-        if args.concat_tokens:
+        if concat_tokens:
             embed_dim = 768 * 2
 
         assert matrix.size() == (M, embed_dim), "Huh?"
@@ -370,65 +315,177 @@ if __name__ == "__main__":
 
         output = []  # initialize an empty list to store the output
 
-        for item, vector in colloc2BERT.items():
+        for item, vector in list(colloc2BERT.items()):
             # vector = colloc2BERT['forget dream']
-            act, rt = minz.recognize(
-                vector.to(device), k=args.minerva_k, maxiter=args.minerva_max_iter
-            )
-            output.append([item, act.detach().cpu(), rt])
+            act, rt = minz.recognize(vector.to(device), k=minerva_k, maxiter=minerva_max_iter)
+            output.append([item, act.detach().cpu().item(), rt])
             print(
                 f"Participant {p+1} \t| Seed {s}\t | Running on {device} \t| {output[-1] if output else ''}"
             )
 
-            # set up a dataframe to write the current results to a uniquely-named CSV file
+            # # set up a dataframe to write the current results to a uniquely-named CSV file
 
-            results_l1 = pd.DataFrame(
-                data={
-                    "mode": "l1",
-                    "id": [s],
-                    "participant": [p + 1],
-                    "item": [item],
-                    "act": [act.item()],
-                    "rt": [rt],
-                }
-            )
+            # results_l1 = pd.DataFrame(
+            #     data={
+            #         "mode": "l1",
+            #         "id": [s],
+            #         "participant": [p + 1],
+            #         "item": [item],
+            #         "act": [act.item()],
+            #         "rt": [rt],
+            #     }
+            # )
 
-            with FileLock(out_filename + ".lock"):
-                if not os.path.exists(out_filename):
-                    # delete the file if it exists and write the dataframe with column names
-                    # to the top of the new file
-                    results_l1.to_csv(out_filename, mode="w", header=True, index=False)
-                else:
-                    # append the dataframe to the existing file without column names
-                    results_l1.to_csv(out_filename, mode="a", header=False, index=False)
+            # with FileLock(out_filename + ".lock"):
+            #     if not os.path.exists(out_filename):
+            #         # delete the file if it exists and write the dataframe with column names
+            #         # to the top of the new file
+            #         results_l1.to_csv(out_filename, mode="w", header=True, index=False)
+            #     else:
+            #         # append the dataframe to the existing file without column names
+            #         results_l1.to_csv(out_filename, mode="a", header=False, index=False)
 
         print(
             f" Done with Participant {p+1} | Seed {s}  \n----------------------------------",
             flush=True,
         )
-        return output
+        results_df = pd.DataFrame(
+            data=output,
+            columns=["item", "act", "rt"],
+        )
+        # results_df["mode"] = "l1"
+        results_df["id"] = s
+        results_df["participant"] = p + 1
 
-    NUM_WORKERS = args.num_workers
+        return results_df
+
+    NUM_WORKERS = min(num_workers, num_participants)
 
     if torch.cuda.is_available():
         n_gpus = torch.cuda.device_count()
         worker_devices = [torch.device(i) for i in range(n_gpus)]
         worker_devices = worker_devices * int(np.ceil(NUM_WORKERS / n_gpus))
+    # elif torch.has_mps:
+    #     worker_devices = ["mps"] * NUM_WORKERS
     else:
         worker_devices = ["cpu"] * NUM_WORKERS
     # device = "cuda" if torch.cuda.is_available() else "cpu"
     print(f"Using devices: {worker_devices}")
 
-    out_file = f"results/results-{Path(args.dataset_to_use).name[:-4]}-{args.num_participants}p-lang_{args.space_lang}-freq_{args.frequency_lang}{f'-mix{args.freq_fraction_pt}' if args.frequency_lang == 'mix' else ''}{'-concat' if args.concat_tokens else ''}-m2k_{args.minerva_k}-m2mi_{args.minerva_max_iter}{'-' + args.label if args.label else ''}.csv"
-    if os.path.exists(out_file):
-        os.remove(out_file)
-    if os.path.exists(out_file + ".lock"):
-        os.remove(out_file + ".lock")
+    # if os.path.exists(out_file):
+    #     os.remove(out_file)
+    # if os.path.exists(out_file + ".lock"):
+    #     os.remove(out_file + ".lock")
 
     results = Parallel(n_jobs=NUM_WORKERS, backend="threading")(
-        delayed(iter)(p, s, out_file, worker_devices[p % NUM_WORKERS]) for p, s in enumerate(seed)
+        delayed(iter)(p, s, worker_devices[p % NUM_WORKERS]) for p, s in enumerate(seed)
     )
-    if os.path.exists(out_file + ".lock"):
-        os.remove(out_file + ".lock")
 
-    print("********************************\n\nAll done!\n\n********************************")
+    results_df = pd.concat(results, ignore_index=True)
+    results_df["space_lang"] = space_lang
+    results_df["frequency_lang"] = frequency_lang
+    results_df["minerva_k"] = minerva_k
+    results_df["minerva_max_iter"] = minerva_max_iter
+    results_df["freq_fraction_pt"] = freq_fraction_pt if frequency_lang == "mix" else -1
+
+
+    return results_df
+
+    # if os.path.exists(out_file + ".lock"):
+    #     os.remove(out_file + ".lock")
+
+    print("****************************\n\nAll done!\n\n****************************")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--dataset_to_use", help="Dataset to use", default="data/stimuli.csv")
+    parser.add_argument(
+        "-l",
+        "--space_lang",
+        help="Use embeddings from this language space (en, pt, en_aligned)",
+        default="en",
+        choices=["en", "pt", "en_aligned"],
+    )
+    parser.add_argument(
+        "-f",
+        "--frequency_lang",
+        help="Use frequency counts from this language (en, pt or mix)",
+        default="en",
+        choices=["en", "pt", "mix"],
+    )
+    parser.add_argument(
+        "-n",
+        "--num_participants",
+        help="How many participants to model?",
+        default=99,
+        type=int,
+    )
+    parser.add_argument(
+        "--en_pt_trans_pickle",
+        help="Path to pickle file containing pt translations of en embeddings (required for aligned space_lang)",
+        default=None,
+    )
+    parser.add_argument(
+        "--freq_fraction_pt",
+        help="Use this fraction of PT in frequency mixing (only applicable for aligned space_lang, default 0.6)",
+        default=0.6,
+        type=float,
+    )
+    parser.add_argument(
+        "--minerva_k",
+        help="Minerva k (threshold) parameter",
+        default=0.955,
+        type=float,
+    )
+    parser.add_argument(
+        "--minerva_max_iter",
+        help="Minerva max_iter parameter",
+        default=450,
+        type=int,
+    )
+    parser.add_argument(
+        "--num_workers",
+        help="Number of workers to use",
+        default=4,
+        type=int,
+    )
+    parser.add_argument(
+        "--no_concat_tokens",
+        dest="concat_tokens",
+        help="Concatenate BERT tokens instead of averaging",
+        action="store_false",
+        default=True,
+    )
+    parser.add_argument(
+        "--label",
+        help="Arbitrary label to append to all files created",
+        default=None,
+    )
+    parser.add_argument(
+        "--append_to_file",
+        help="Filename of existing csv file to append results to",
+        type=str,
+        default=None,
+    )
+    args = parser.parse_args()
+
+    results_df = run_experiment(
+        dataset_to_use=args.dataset_to_use,
+        space_lang=args.space_lang,
+        frequency_lang=args.frequency_lang,
+        num_participants=args.num_participants,
+        en_pt_trans_pickle=args.en_pt_trans_pickle,
+        freq_fraction_pt=args.freq_fraction_pt,
+        minerva_k=args.minerva_k,
+        minerva_max_iter=args.minerva_max_iter,
+        num_workers=args.num_workers,
+        concat_tokens=args.concat_tokens,
+        label=args.label,
+    )
+
+    if args.append_to_file:
+        results_df.to_csv(args.append_to_file, mode="a", header=False, index=False)
+    else:
+        out_file = f"results/results-{Path(args.dataset_to_use).name[:-4]}-{args.num_participants}p-lang_{args.space_lang}-freq_{args.frequency_lang}{f'-mix{args.freq_fraction_pt}' if args.frequency_lang == 'mix' else ''}{'-concat' if args.concat_tokens else ''}-m2k_{args.minerva_k}-m2mi_{args.minerva_max_iter}{'-' + args.label if args.label else ''}.csv"
+        results_df.to_csv(out_file, index=False)
