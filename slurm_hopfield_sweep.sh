a6b7673b4d8b3daa4147212424a7e797e3d5fea3
Ivan Vegner <ivan.vegner@ed.ac.uk>
2025-06-14 20:13:09 +0100

    Add slurm script

---
+++ slurm_hopfield_sweep.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+#SBATCH --job-name=MINERVA_hopfield_sweep
+#SBATCH --partition=ILCC-Standard
+#SBATCH --nodelist=duflo
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=8000
+#SBATCH --gres=gpu:1
+#SBATCH --time=48:00:00
+#SBATCH --output="/home/%u/minerva2/slurm_logs/%x_%A-%a.out"
+#dummy-SBATCH --error="/home/%u/minerva2/slurm_logs/%x_%A-%a.err"
+
+#SBATCH --signal=B:TERM@120 # tells the controller
+                            # to send SIGTERM to the job 60 secs
+                            # before its time ends to give it a
+                            # chance for better cleanup.
+
+
+module load miniconda/3
+
+nvidia-smi
+
+# =====================
+# Logging information
+# =====================
+
+# slurm info - more at https://slurm.schedmd.com/sbatch.html#lbAJ
+echo "Job running on ${SLURM_JOB_NODELIST}"
+
+dt=$(date '+%d/%m/%Y %H:%M:%S')
+echo "Job started: $dt"
+
+
+# ===================
+# Environment setup
+# ===================
+
+echo "Setting up bash enviroment"
+
+# Make available all commands on $PATH as on headnode
+source ~/.bashrc
+
+# Make script bail out after first error
+set -e
+
+# Make your own folder on the node's scratch disk
+# N.B. disk could be at /disk/scratch_big, or /disk/scratch_fast. Check
+# yourself using an interactive session, or check the docs:
+#     http://computing.help.inf.ed.ac.uk/cluster-computing
+# SCRATCH_DISK=/disk/scratch2
+# SCRATCH_DISK=/disk/scratch
+# SCRATCH_HOME=${SCRATCH_DISK}/${USER}
+# SCRATCH_PROJECT_DIR=${SCRATCH_HOME}/minerva2/wandb
+# SCRATCH_DATA_DIR=${SCRATCH_PROJECT_DIR}/data
+# mkdir -p ${SCRATCH_DATA_DIR}
+
+# Activate your conda environment
+CONDA_ENV_NAME=minerva
+echo "Activating conda environment: ${CONDA_ENV_NAME}"
+conda activate ${CONDA_ENV_NAME}
+conda list
+echo "Environment Activated!"
+
+# Install trap for the signals INT and TERM to
+ # the main BATCH script here.
+ # Send SIGTERM using kill to the internal script's
+ # process and wait for it to close gracefully.
+
+ # Note: Most python scripts don't install handler
+ # for SIGTERM and hence might die a quick painful death
+ # on recieveing SIGTERM (kill -15).
+ # To avoid this, you can send SIGINT,
+ # i.e., KeyboardInterrupt using (kill -2).
+trap 'echo signal received in BATCH!; kill -15 "${PID}"; wait "${PID}";' SIGINT SIGTERM
+
+# start script in background and get its PID
+# Note: $* traps all arguments passed to the script
+WANDB_DIR=/disk/scratch/s2191163/minerva_wandb python src/run_hopfield_sweep.py $*
+
+# Set the PID var so that the trap can use it
+PID="$!"
+wait "${PID}"
+
+echo "The end!"
+# =========================
+# Post experiment logging
+# =========================
+echo ""
+echo "============"
+echo "job finished successfully"
