f5792f7292f458a4dfcbe537818ae72d0da2c872
Ivan Vegner <ivan.vegner@ed.ac.uk>
2025-06-15 20:42:19 +0100

    remove seed

--- src/run_hopfield_sweep.py
+++ src/run_hopfield_sweep.py
@@ -31,11 +31,11 @@ def run():
     "If --memories, models will be trained with explicit noisy memories.",
 )
 def run_sweep(sweep_id: str, lookup: bool):
-    seed = 0
+    # seed = 0
 
-    random.seed(seed)
-    torch.manual_seed(seed)
-    np.random.seed(seed)
+    # random.seed(seed)
+    # torch.manual_seed(seed)
+    # np.random.seed(seed)
 
     # 2: Define the search space
 
@@ -44,7 +44,8 @@ def run_sweep(sweep_id: str, lookup: bool):
         c = {
             "batch_size": {"values": [64]},
             "num_epochs": {"values": [num_epochs]},
-            "memory_size": {"values": [3] + list(range(10, 100, 20)) + list(range(100, 1000, 100))},
+            # "memory_size": {"values": [3] + list(range(10, 100, 20)) + list(range(100, 1000, 100))},
+            "memory_size": {"min": 3, "max": 1000},
             "learn_lookup": {"value": True},
             "lookup_n_train_samples": {"values": [1000, 5000, 10000]},
         }
@@ -53,13 +54,13 @@ def run_sweep(sweep_id: str, lookup: bool):
         c = {
             "batch_size": {"values": [16]},
             "num_epochs": {"values": [num_epochs]},
-            "memory_size": {"values": [246, 500, 1000, 5000, 10000]},
+            "memory_size": {"min": 246, "max": 10000},
             "learn_lookup": {"value": False},
             "lookup_n_train_samples": {"value": None}
         }
 
     sweep_configuration = {
-        "method": "grid",
+        "method": "bayes",
         "metric": {"goal": "maximize", "name": "score"},
         "parameters": {
             "dataset_to_use": {"value": "data/stimuli_idioms_clean.csv"},
@@ -69,9 +70,13 @@ def run_sweep(sweep_id: str, lookup: bool):
             "avg_last_n_layers": {"value": 1},
             "num_participants": {"value": 1},
             "num_workers": {"value": 1},
-            "forget_prob": {"values": [0.0, 0.2, 0.4, 0.6, 0.8]},
-            "minerva_k": {"values": [0.95, 0.96, 0.97, 0.98, 0.99, 0.995]},
-            "hidden_size": {"values": [50, 100, 200, 300, 400, 500, 600, 700, 768]},
+            "loss": {"value": "cosine"},
+            # "forget_prob": {"values": [0.0, 0.2, 0.4, 0.6, 0.8]},
+            "forget_prob": {"min": 0.0, "max": 0.8},
+            # "minerva_k": {"values": [0.95, 0.96, 0.97, 0.98, 0.99, 0.995]},
+            "minerva_k": {"min": 0.95, "max": 0.995},
+            # "hidden_size": {"values": [50, 100, 200, 300, 400, 500, 600, 700, 768]},
+            "hidden_size": {"min": 50, "max": 768},
             **c
         },
         "early_terminate": {
